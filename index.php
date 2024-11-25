426929449fad70e190af997615a4d1eeb164adef
ismail hallac <mavi-okyanus63@hotmail.com>
2024-11-25 22:49:13 +0300

    pretty url added

--- index.php
+++ index.php
@@ -1,18 +1,11 @@
 <?php
 
+$path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$segments = explode('/',$path);
+$action = $segments[3];
+$controller = $segments[2];
 
-$action = $_GET["action"];
-$controller = $_GET["controller"];
+require "src/controllers/$controller.php";
+$controller_object = new $controller;
 
-if($controller === "home"){
-    require 'src/controllers/home.php';
-    $controller_object = new Home;
-} else if($controller === "products"){
-    require 'src/controllers/products.php';
-    $controller_object = new Products;
-}
-if ($action === "index") {
-    $controller_object->index();
-} else if ($action === "show") {
-    $controller_object->show();
-}
+$controller_object->$action();
