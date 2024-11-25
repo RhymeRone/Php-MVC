acb00e2c7b6727b5f22e02676ff93e7a496673a0
ismail hallac <mavi-okyanus63@hotmail.com>
2024-11-26 00:38:50 +0300

    routes added

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
-$path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-$segments = explode('/',$path);
-$action = $segments[3];
-$controller = $segments[2];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+require "src/router.php";
+$router = new Router;
+$router->add("/mvc/home/index", ['controller' => 'home', 'action' => 'index']);
+$router->add("/mvc/products/index", ['controller' => 'products', 'action' => 'index']);
+$router->add('/mvc/', ['controller' => 'home', 'action' => 'index']);
+
+$params = $router->match($path);
+
+if ($params === false) {
+    exit("no route");
+}
+
+$action = $params['action'];
+$controller = $params['controller'];
 
 require "src/controllers/$controller.php";
 $controller_object = new $controller;
